d0e6aaa589016e46c841a8382f5fafb8c22acc74
Edd <eduardo940531@gmail.com>
2021-01-04 12:57:30 -0500

    Navbar with fontawesome icon

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to='/' className="navbar-logo">
+            EVP <i className="fab fa-battle-net" />  
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              
+          </ul>    
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
